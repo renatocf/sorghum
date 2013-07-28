fcf040649f7e1092e64cece4a74ada42476f9dbe
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-07-28 19:24:35 -0300

    Changing the .bash to work with the changes in the .pl file

--- venn.bash
+++ venn.bash
@@ -5,9 +5,5 @@ echoerr() { echo "$@" 1>&2; }
 DATE=2013_07_26
 VENN=nucleotide_exon_with_intron_partial_venn.txt 
 
-for i in $(seq -f %02.0f 1 10); 
-do
-    DIR=CHR_${i}/sgeval_${DATE}_chr${i}
-    cat ${DIR}/${VENN} | perl venn.pl
-    echo;
-done
+DIR=CHR_*/sgeval_${DATE}_chr*
+perl venn.pl ${DIR}/${VENN} 

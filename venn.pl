50a4fd10d7f502000cdace7ebb9158d1b4a295bb
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-07-28 19:23:46 -0300

    Adding Alan's script tips and changing it to receive as regex argument all the files

--- venn.pl
+++ venn.pl
@@ -3,42 +3,73 @@
 use strict;
 use warnings;
 
-my $i = undef;
-my $j = undef;
-my $exp = undef;
-my $group = undef;
-my $number = undef;
+my %hash = ();
+my $key_msize = 0;
+my $num_msize = 0;
 
-while($exp = <>) 
-{ 
-    unless($exp =~ /^\t|\/\//) 
+# Getting all files to be processed
+foreach my $file (@ARGV)
+{
+    open(FILE, "<", $file);
+    while(my $exp = <FILE>) 
     { 
+        # All the // or sequences are taken off
+        next if($exp =~ /^\t|\/\//);
         chomp $exp; # Taking out '\n'
         
         # Splitting the sentence in 2 parts: groups and numbers
-        ($group, $number) = split("\t", $exp); 
+        my ($key, $number) = split("\t", $exp); 
         
-        # Firt part is printed 'as it'
-        print "$group\t"; 
+        # Editing keys for making them more 'readable'
+        $key =~ s/\_chr\d\d//g;
+        $key =~ s/\.clean//g;
+        $key =~ s/Sb.*pasa/pasa/g;
         
-        # Creates an array with the numeric part of the sentence.
-        # The aim is to make more legible numbers
-        my @num = split("", $number);
-        my $size = scalar @num;
+        # Splits fields and sort them
+        my @fields = split(/\|/, $key);
+        $key = join("|",sort(@fields));
         
-        # Last digit multiple of 3
-        my $dif = $size % 3;
+        # Stores in the hash
+        $hash{$key} += $number;
         
-        # Prints untill reaches the last digit multiple of 3
-        # and, then, to each multiple, prints a '.'
-        # E.g. 31415 - the last multiple is 4 and it prints 31,
-        #              then puts a '.' and prints 415
-        for($j = -$dif, $i = 0; $i != $size; $i++, $j++) 
-        { 
-            print "." unless( ($j % 3) > 0 | !$i );
-            print $num[$i]; 
-        }
-        
-        print "\n";
-    } 
+        # Takes the biggest size of the keys to print it later
+        my $s = length $key;
+        ($s > $key_msize) ? ($key_msize = $s) : ();
+    }
+    close FILE;
+}
+
+# Process all numbers in a more legible format
+foreach my $key (keys(%hash))
+{
+    # Creates an array with the numeric part of the sentence.
+    # The aim is to make more legible numbers
+    my @num = split("", $hash{$key});
+    my $size = scalar @num;
+    
+    # Last digit multiple of 3
+    my $dif = $size % 3;
+    
+    # Prints untill reaches the last digit multiple of 3
+    # and, then, to each multiple, prints a '.'
+    # E.g. 31415 - the last multiple is 4 and it prints 31,
+    #              then puts a '.' and prints 415
+    $hash{$key} = "";
+    for(my $j = -$dif, my $i = 0; $i != $size; $i++, $j++) 
+    { 
+        $hash{$key} .= "." unless( ($j % 3) > 0 || !$i );
+        $hash{$key} .= "$num[$i]"; 
+    }
+    
+    # Takes the biggest size of the numbers to print it later
+    my $s = length $hash{$key};
+    ($s > $num_msize) ? ($num_msize = $s) : ();
+}
+
+foreach my $key (sort keys(%hash)) 
+{
+    # Prints all the keys in the following format:
+    # pred1|pred2|...|predn => 23.543.354.134
+    printf "%-*s => %*s\n", $key_msize, $key, 
+                            $num_msize, $hash{$key};
 }

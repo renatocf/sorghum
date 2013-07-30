6d218ba2b269c2f2df4fb8bc37f182e7fe7f2984
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-07-29 23:56:35 -0300

    Adding a script to print the same log as in nucleotides.bash, but with all files together

---
+++ nucleotides_together.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+echolog() { echo "$@"; } #1>results.txt; }
+echoerr() { echo "$@" 1>&2; }
+
+echolog "Relatório de nucleotídeos"
+echolog
+
+MYOP=myop_chr*.gtf.clean.new 
+AUGUSTUS=augustus_chr*.gtf.clean.new 
+VENN=nucleotide_exon_with_intron_partial_venn.txt 
+ACCURACY=nucleotide_exon_with_intron_partial_accuracy.txt 
+
+echolog "Somando a partir do SGEval:"
+perl venn.pl CHR_*/sgeval_2013_07_29_chr*/${VENN} \
+    | perl nucleotide.pl 2> /dev/null
+echolog
+
+echolog "Contando CDS:"
+perl count_bases.pl CHR_*/${MYOP} CHR_*/${AUGUSTUS} 2> /dev/null
+echolog

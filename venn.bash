812709f98c8d74375145858113f09a4bb8467087
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-08-01 22:35:57 -0300

    Changing date and adding accuracy.pl to print accuracy info about the predictors that are in the venn.pl relatory

--- venn.bash
+++ venn.bash
@@ -2,8 +2,10 @@
 echolog() { echo "$@"; }
 echoerr() { echo "$@" 1>&2; }
 
-DATE=2013_07_26
+DATE=2013_07_30
 VENN=nucleotide_exon_with_intron_partial_venn.txt 
 
 DIR=CHR_*/sgeval_${DATE}_chr*
 perl venn.pl ${DIR}/${VENN} 
+echo 
+perl venn.pl ${DIR}/${VENN} | perl accuracy.pl 2> /dev/null

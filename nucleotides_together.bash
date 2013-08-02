f20842ce351cbc99ef03bf1adfcb922f3fad4173
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-08-01 22:36:59 -0300

    Separating date and putting it in a variable

--- nucleotides_together.bash
+++ nucleotides_together.bash
@@ -5,13 +5,14 @@ echoerr() { echo "$@" 1>&2; }
 echolog "Relatório de nucleotídeos"
 echolog
 
+DATE=2013_07_30
 MYOP=myop_chr*.gtf.clean.new 
 AUGUSTUS=augustus_chr*.gtf.clean.new 
 VENN=nucleotide_exon_with_intron_partial_venn.txt 
 ACCURACY=nucleotide_exon_with_intron_partial_accuracy.txt 
 
 echolog "Somando a partir do SGEval:"
-perl venn.pl CHR_*/sgeval_2013_07_29_chr*/${VENN} \
+perl venn.pl CHR_*/sgeval_${DATE}_chr*/${VENN} \
     | perl nucleotide.pl 2> /dev/null
 echolog
 

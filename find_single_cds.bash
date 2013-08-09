b71552321d8f89a44d60de24bb879606e2b0887c
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-08-08 21:36:25 -0300

    Adding script for finding single CDS predictions in a .gtf files.

---
+++ find_single_cds.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+DATE=2013_07_08
+perl find_single_cds.pl -a CHR_*/Sb_${DATE}_chr*_pasa.gtf
---
+++ find_single_cds.pl
@@ -0,0 +1,111 @@
+#!/usr/bin/perl
+use v5.10;
+
+#######################################################################
+# Program:    correct_id;pl                                           #
+# mantainer:  Renato Cordeiro Ferreira                                #
+# usage:      This program gets .gtf files and prints the amount of   #
+#             single CDS predictions found. The options -a prints     #
+#             a relatory with all individual files data together.     #
+#             -g creates 2 new files, each one with the single cds    #
+#             and all the other predictions.                          #
+# date:       08/08/13 (dd/mm/yy)                                     #
+#######################################################################
+
+# Pragmas
+use strict;
+use warnings;
+use Getopt::Long;
+
+## HELP/USAGE #########################################################
+# Options
+Getopt::Long::Configure('bundling');
+my $all = undef;
+my $gen = undef;
+
+GetOptions(
+    'a|all-together'  => \$all,
+    'g|generate-file' => \$gen,
+);
+
+my $usage_message = << "USAGE_MESSAGE";
+    USAGE: perl find_single_cds.pl [-a] [-g] <file1.gtf> ...
+USAGE_MESSAGE
+
+if(scalar(@ARGV) <= 0) {
+    die "\n$usage_message\n";
+}
+
+## GLOBAL VARIABLES ###################################################
+my $total = 0;
+my $good = 0;
+my $bad = 0;
+my $all_good = 0;
+my $all_bad = 0;
+
+local $/ = "\n\n";
+
+## SCRIPT #############################################################
+FILE: for my $file (@ARGV)
+{
+    $total = $good = $bad = 0;
+    
+    # Open file for being read
+    open(my $FILE, "<:utf8", $file)
+        or die "Problems to open $file: $!";
+    
+    # Create 2 files: .multiple for good proteins
+    # and .single for bad proteins
+    my ($GOOD, $BAD) = (undef, undef);
+    if($gen) {
+        open($GOOD, ">:utf8", "$file.multiple") 
+            or die "Problems to open $file.multiple: $!";
+        open($BAD,  ">:utf8", "$file.single")
+            or die "Problems to open $file.single: $!";
+    }
+    
+    LINE: while(my $line = <$FILE>)
+    {
+        $total++;
+        my @pred = split("\n", $line);
+        # say STDERR "DEBUG:SIZE ==> ", scalar @pred;
+        
+        # Separate singe-cds predictions in the STDERR
+        if(scalar @pred <= 3) {
+            print $BAD "$line" if $gen;
+            $bad++; $all_bad++;
+        } else {
+            print $GOOD "$line" if $gen;
+            $good++; $all_good++;
+        }
+    }
+    
+    my $bad_percent = 100*$bad/$total;
+    my $good_percent = 100*$good/$total;
+    
+    printf 
+    "$file:\n".
+    "Single:   $bad (%2.2f%%)\n".
+    "Multiple: $good (%2.2f%%)\n".
+    "Total:    $total\n".
+    "//\n", 
+    $bad_percent, $good_percent;
+    
+    FH: for my $fh ($FILE, $GOOD, $BAD) { 
+        close $fh if defined $fh;
+    }
+}
+
+if($all)
+{
+    my $bad_percent = 100*$bad/$total;
+    my $good_percent = 100*$good/$total;
+    
+    printf << "REPORT_ALL", $bad_percent, $good_percent;
+all_together:
+    Single:   $all_bad (%2.2f%%)
+    Multiple: $all_good (%2.2f%%)
+    Total:    $total
+//
+REPORT_ALL
+}

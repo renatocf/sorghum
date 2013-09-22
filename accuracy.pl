8b800dfaad4cfdb4f0a258310d5cbf57ed156038
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-09-22 12:58:44 -0300

    Changing the way numbers are writen in a more legible format

--- accuracy.pl
+++ accuracy.pl
@@ -106,43 +106,31 @@ foreach my $pred (keys(%hash))
     # were predicted by this predictor - def. of False Positive)
     $hash{$pred}{'TN'} = $pasa_intron - $hash{$pred}{'FP'};
     
-    foreach my $key (keys %{$hash{$pred}})
+    # Process all numbers in a more legible format
+    foreach (values %{$hash{$pred}})
     {
-        # Creates an array with the numeric part of the sentence.
-        # The aim is to make more legible numbers
-        my @num = split("", $hash{$pred}{$key});
-        my $size = scalar @num;
-        
-        # Last digit multiple of 3
-        my $dif = $size % 3;
-        
-        # Prints untill reaches the last digit multiple of 3
-        # and, then, to each multiple, prints a '.'
-        # E.g. 31415 - the last multiple is 4 and it prints 31,
-        #              then puts a '.' and prints 415
-        $hash{$pred}{$key} = "";
-        for(my $j = -$dif, my $i = 0; $i != $size; $i++, $j++) 
-        { 
-            $hash{$pred}{$key} .= "." unless( ($j % 3) > 0 || !$i );
-            $hash{$pred}{$key} .= "$num[$i]"; 
-        }
-        
-        # Takes the biggest size of the numbers to print it later
-        my $s = length $hash{$pred}{$key};
-        ($s > $num_msize) ? ($num_msize = $s) : ();
+        my $size = 0;
+        $size += 4 while(s/(.*)(\d)(\d{3})/$1$2.$3/);
+        $size += 1 + length $1;
+        ($size > $num_msize) ? ($num_msize = $size) : ()
     }
+
+    my ($TP, $TN) = ($hash{$pred}{'TP'}, $hash{$pred}{'TN'});
+    my ($FP, $FN) = ($hash{$pred}{'FP'}, $hash{$pred}{'FN'});
+    
+    # Sensibility: True positive / All positives
+    # Specificity: True negative / All negatives
+    say "TN==:> $TN\n", "FP==:> $FP";
+    say $hash{$pred}{'sensibility'} = $TP/($TP+$FN); 
+    say $hash{$pred}{'specificity'} = $TN/($TN+$FP); 
 }
 
 foreach my $pred (sort keys %hash) 
 {
-    print "$pred\n";
-    (my $TP = $hash{$pred}{'TP'}) =~ s/\.//g;
-    (my $TN = $hash{$pred}{'TN'}) =~ s/\.//g;
-    (my $FP = $hash{$pred}{'FP'}) =~ s/\.//g;
-    (my $FN = $hash{$pred}{'FN'}) =~ s/\.//g;
-    my $sensibility = $TP/($TP+$FN); # True positive / All positives
-    my $specificity = $TN/($TN+$FP); # True negative / All negatives
+    my $sensibility = delete $hash{$pred}{'sensibility'};
+    my $specificity = delete $hash{$pred}{'specificity'};
     
+    print "$pred\n";
     foreach my $key (keys %{$hash{$pred}})
     {
         # Prints all the keys in the following format:

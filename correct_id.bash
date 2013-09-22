58800c485c5831ef48b9e1332055a1121cc92998
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-09-22 09:51:56 -0300

    Adding manteinance and usage info in the topof the comments. Also adding 'echolog' and 'echoerr' aliases.

--- correct_id.bash
+++ correct_id.bash
@@ -1,4 +1,15 @@
 #!/bin/bash
+echolog() { echo "$@"; }
+echoerr() { echo "$@" 1>&2; }
+
+#######################################################################
+# Program:    correct_id.bash                                         #
+# mantainer:  Renato Cordeiro Ferreira                                #
+# usage:      This program runs correct_id.pl for all gtf, for the    #
+#             two predictors AUGUSTUS and MYOP, generating as output  #
+#             gtf files with the ids corrected (with etension .new).  #
+# date:       26/07/13 (dd/mm/yy)                                     #
+#######################################################################
 
 # Myop predictor
 perl correct_id.pl MYOP CHR*/myop_chr*.gtf.clean
--- find_single_cds.bash
+++ find_single_cds.bash
@@ -1,4 +1,15 @@
 #!/bin/bash
+echolog() { echo "$@"; }
+echoerr() { echo "$@" 1>&2; }
+
+#######################################################################
+# Program:    find_single_cds.bash                                    #
+# mantainer:  Renato Cordeiro Ferreira                                #
+# usage:      Runs find_single_cds.bash over all pasa, augustus and   #
+#             myop gtfs for generating .single and .multiple files    #
+#             with the predictions which have 1 or more CDS.          #
+# date:       06/08/13 (dd/mm/yy)                                     #
+#######################################################################
 
 DATE=2013_07_08
 perl find_single_cds.pl -a CHR_*/Sb_${DATE}_chr*_pasa.gtf

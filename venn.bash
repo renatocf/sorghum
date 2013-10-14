fbd2b4d38c1dd8067b8c2895e3b4615508391e3b
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-10-14 07:03:51 -0300

    Creating variables dor file names

--- venn.bash
+++ venn.bash
@@ -7,32 +7,36 @@ echoerr() { echo "$@" 1>&2; }
 # mantainer:  Renato Cordeiro Ferreira                                #
 # usage:      Runs venn.pl twice: the first, for creating all the     #
 #             textual venn diagram; the second, pipeing the output    #
-#             for getting the data with accuracy.pl.                  #
+#             for getting the data with accuracy.pl. The files are    #
+#             generated for all nucleotides, exon nucleotides and     #
+#             gene structures.                                        #
 # date:       26/07/13 (dd/mm/yy)                                     #
 #######################################################################
 
 ## PREAMBLE ###########################################################
 DATE=2013_09_22
-VENN=nucleotide_exon_with_intron_partial_venn.txt 
 DIR=CHR_*/sgeval_${DATE}_chr*
+NUCL=nucleotide_exon_with_intron_partial_venn.txt 
+EXON=nucleotide_exon_venn.txt
+GENE=gene_exact_venn.txt
 
 ## SCRIPT #############################################################
 echolog "== NUCLEOTIDES =============================================="
 echolog
-perl venn.pl ${DIR}/${VENN} 
+perl venn.pl ${DIR}/${NUCL} 
 echolog
-perl venn.pl ${DIR}/${VENN} | perl accuracy.pl 2> /dev/null
+perl venn.pl ${DIR}/${NUCL} | perl accuracy.pl 2> /dev/null
 
 echolog
 echolog "== EXON ====================================================="
 echolog
-perl venn.pl ${DIR}/nucleotide_exon_venn.txt
+perl venn.pl ${DIR}/${EXON}
 echolog
-perl venn.pl ${DIR}/nucleotide_exon_venn.txt | perl accuracy.pl 2> /dev/null
+perl venn.pl ${DIR}/${EXON} | perl accuracy.pl 2> /dev/null
 
 echolog
 echolog "== GENE ====================================================="
 echolog
-perl venn.pl ${DIR}/gene_exact_venn.txt
+perl venn.pl ${DIR}/${GENE}
 echolog
-perl venn.pl ${DIR}/gene_exact_venn.txt | perl accuracy.pl 2> /dev/null
+perl venn.pl ${DIR}/${GENE} | perl accuracy.pl 2> /dev/null

73834fba3b2c9fbc193e80fa81371c9b0877ed42
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-10-22 07:50:53 -0200

    Adding new script to the workflow

--- stop_codons.bash
+++ stop_codons.bash
@@ -13,41 +13,43 @@ echoerr() { echo "$@" 1>&2; }
 # date:       21/10/13 (dd/mm/yy)                                     #
 #######################################################################
 
-# Runs stop_codon.pl to get the predictor
-# sequences from the input.
-for i in $(seq -f %02.0f 10); 
-do 
-    echolog "CHR_${i}"
-    myop-gtf_to_cds.pl                      \
-    -g CHR_${i}/myop_chr${i}.gtf.clean.new  \
-    -f CHR_${i}/CHR_${i}_RefSeq.fasta       \
-    1> CHR_${i}/myop_chr${i}.fasta
-done 
-
-# Runs transeq for generating chromossomes
-for i in $(seq -f %02.0f 10); 
-do 
-    transeq CHR_${i}/myop_chr${i}.fasta \
-    -outseq CHR_${i}/myop_chr${i}.pep
-done
-
-# Count number of stop codons
-echolog "Counting number of stop codons"
-for i in $(seq -f %02.0f 10); 
-do
-    echolog "CHR_${i}"
-    perl stop_codons.pl                 \
-    <  CHR_${i}/myop_chr${i}.pep        \
-    1> CHR_${i}/myop_chr${i}.pep.long   \
-    2> CHR_${i}/myop_chr${i}.pep.short
-done
+# # Runs stop_codon.pl to get the predictor
+# # sequences from the input.
+# for i in $(seq -f %02.0f 10); 
+# do 
+#     echolog "CHR_${i}"
+#     myop-gtf_to_cds.pl                      \
+#     -g CHR_${i}/myop_chr${i}.gtf.clean.new  \
+#     -f CHR_${i}/CHR_${i}_RefSeq.fasta       \
+#     1> CHR_${i}/myop_chr${i}.fasta
+# done 
+# 
+# # Runs transeq for generating chromossomes
+# for i in $(seq -f %02.0f 10); 
+# do 
+#     transeq CHR_${i}/myop_chr${i}.fasta \
+#     -outseq CHR_${i}/myop_chr${i}.pep
+# done
+# 
+# # Count number of stop codons
+# echolog "Counting number of stop codons"
+# for i in $(seq -f %02.0f 10); 
+# do
+#     echolog "CHR_${i}"
+#     perl stop_codons.pl                 \
+#     <  CHR_${i}/myop_chr${i}.pep        \
+#     1> CHR_${i}/myop_chr${i}.pep.long   \
+#     2> CHR_${i}/myop_chr${i}.pep.short
+# done
 
 # Creates stop_codon.data with the results.
 echo > stop_codons.data
+echo "Stop codon statistics"         >> stop_codons.data
+echo "=============================" >> stop_codons.data
 for i in $(seq -f %02.0f 10); 
 do
-    echo                                         >> stop_codons.data
-    echo "CHR_${i} ============================" >> stop_codons.data
-    seqstat CHR_${i}/myop_chr${i}.pep            >> stop_codons.data
-    seqstat CHR_${i}/myop_chr${i}.pep.short      >> stop_codons.data
+    perl stop_codons_analyse.pl     \
+    CHR_${i}/myop_chr${i}.pep       \
+    CHR_${i}/myop_chr${i}.pep.short \
+    >> stop_codons.data
 done
